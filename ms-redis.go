fc180870da3cb67b2c129a3d1f33567c04f15cfe
ablingchos <56404402+ablingchos@users.noreply.github.com>
2023-11-08 15:15:19 +0800

    Update ms-redis.go
    将go-redis命名为redis

--- ms-redis.go
+++ ms-redis.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/redis/go-redis"
+	redis "github.com/redis/go-redis"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"go.opentelemetry.io/otel"
